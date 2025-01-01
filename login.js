fd27fef0e908e572c17445487d8d2bfaec374c69
meenakshi25jan <48888640+meenakshi25jan@users.noreply.github.com>
2025-01-01 10:18:17 +0530

    Update login.js

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ document.getElementById('login-form').addEventListener('submit', async function(
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     
-    const response = await fetch('http://localhost:5000/login', {
+    const response = await fetch('http://localhost:5000/Login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
